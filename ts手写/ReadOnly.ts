a98074e3c4db123ed26a5e003ae8f59a422fbba5
郭炯韦 <18530633992@163.com>
2022-07-09 23:00:23 +0800

    feat: 实现ts的ReadOnly方法

---
+++ ts手写/ReadOnly.ts
@@ -0,0 +1,30 @@
+/**
+ * 主要实现是通过映射遍历所有key，
+ * 然后给每个key增加一个readonly修饰符
+ */
+
+interface IPerson {
+  name: string
+  age: number
+  sex: '男' | '女'
+}
+
+// 实现Readonly
+
+// keyof遍历后，添加readonly注解
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
+
+type IPerson1 = MyReadonly<IPerson>
+
+const person1: IPerson1 = {
+  name: '哈哈',
+  age: 20,
+  sex: '男'
+}
+
+// 会报错
+// person1.age = 111
+
+export {}
