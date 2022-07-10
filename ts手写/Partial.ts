1dd77201d85c3b3087230b0f36c6eaae15574929
郭炯韦 <18530633992@163.com>
2022-07-10 22:45:51 +0800

    config: 添加prettier配置保存自动格式化代码

--- ts手写/Partial.ts
+++ ts手写/Partial.ts
@@ -1,4 +1,3 @@
-
 interface IPerson {
   name: string
   age: number
@@ -11,16 +10,15 @@ interface IPerson {
  * 然后将每个属性设置为可选属性
  */
 type MyPartial<T> = {
-  [P in keyof T]?: T[P];
+  [P in keyof T]?: T[P]
 }
 
-type IPerson1 = MyPartial<IPerson>;
+type IPerson1 = MyPartial<IPerson>
 
-const person1: IPerson1  ={
+const person1: IPerson1 = {
   name: '不用指定其他参数'
 }
 console.log(person1)
-
 
 // [P in keyof T]通过映射类型，遍历T上的所有属性
 // ?:设置为属性为可选的
@@ -32,17 +30,16 @@ console.log(person1)
  * keyof T得到的是T的所有key组成的联合类型
  */
 type MyPartialOptional<T, K extends keyof T> = {
-  [P in K]?: T[P];
+  [P in K]?: T[P]
 }
 
 /**
  * @example
  * type Eg1 = { key1?: string; key2?: number }
  */
-type IPerson2 = MyPartialOptional<IPerson, 'name' | 'sex'>;
+type IPerson2 = MyPartialOptional<IPerson, 'name' | 'sex'>
 
-const person2: IPerson1  ={
-}
+const person2: IPerson1 = {}
 
 console.log(person2)
 export {}
